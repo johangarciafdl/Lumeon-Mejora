11d9275f730ea37ff3b635e3f9306bb394be88c2
johangarciafdl <johansebastiangarciavilar@gmail.com>
2026-08-17 18:50:16 -0500

    feat: register v2 auth API

--- lumeon_pro/backend/api/__init__.py
+++ lumeon_pro/backend/api/__init__.py
@@ -1,6 +1,7 @@
 """HTTP/API layer for Lumeon V2."""
 
 from .assistant_api import assistant_api
+from .auth_api import auth_api
 from .customer_api import customer_api
 from .delivery_api import delivery_api
 from .invoice_api import invoice_api
@@ -10,6 +11,7 @@ from .system_api import system_api
 
 def register_blueprints(app):
     app.register_blueprint(assistant_api)
+    app.register_blueprint(auth_api)
     app.register_blueprint(customer_api)
     app.register_blueprint(delivery_api)
     app.register_blueprint(invoice_api)

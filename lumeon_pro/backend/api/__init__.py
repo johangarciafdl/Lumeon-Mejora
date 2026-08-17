e122e22b3b58fd28f8ea4d1d9274a42f04a1cd44
johangarciafdl <johansebastiangarciavilar@gmail.com>
2026-08-17 18:52:38 -0500

    feat: register v2 inventory API

--- lumeon_pro/backend/api/__init__.py
+++ lumeon_pro/backend/api/__init__.py
@@ -4,6 +4,7 @@ from .assistant_api import assistant_api
 from .auth_api import auth_api
 from .customer_api import customer_api
 from .delivery_api import delivery_api
+from .inventory_api import inventory_api
 from .invoice_api import invoice_api
 from .product_api import product_api
 from .system_api import system_api
@@ -14,6 +15,7 @@ def register_blueprints(app):
     app.register_blueprint(auth_api)
     app.register_blueprint(customer_api)
     app.register_blueprint(delivery_api)
+    app.register_blueprint(inventory_api)
     app.register_blueprint(invoice_api)
     app.register_blueprint(product_api)
     app.register_blueprint(system_api)

77d1ed2613a97c3964223f080e7643a5e385087e
johangarciafdl <johansebastiangarciavilar@gmail.com>
2026-08-23 23:16:29 -0500

    fix: load stable sales runtime v6

--- lumeon_pro/frontend/sales_ui_runtime_fix_v2.js
+++ lumeon_pro/frontend/sales_ui_runtime_fix_v2.js
@@ -1,6 +1,6 @@
 (() => {
   'use strict';
   const src = document.createElement('script');
-  src.src = '/sales_ui_runtime_fix_v5.js?v=20260824-1';
+  src.src = '/sales_ui_runtime_fix_v6.js?v=20260824-2';
   document.head.appendChild(src);
 })();
